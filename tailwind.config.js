6574f1a61c6f7483844d46911db6b0cfd0366228
dejosemario <josemariaodumodu@gmail.com>
2024-02-01 21:21:57 +0100

    fix: set up the the colore and fonts needed in the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,50 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    extend:
+    {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "16px",
+          sm: "0",
+          lg: "0",
+          xl: "0",
+          "2xl": "0",
+        },
+        screens: {
+          sm: "540px",
+          md: "720px",
+          lg: "960px",
+          xl: "1140px",
+          "2xl": "1260px",
+          "3xl": "1560",
+        },
+      },
+      screens: {
+        xs: "375px",
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1200px",
+        "2xl": "1440px",
+      },
+      colors: {
+        grey: "#333333",
+        blue: "#3341C1",
+        "grey-light": "#888888",
+        "grey-dark": "#292929",
+        green: "#3EA03B",
+        "grey-999": "#999EA5",
+      },
+      fontFamily: {
+        clash: ["Clash Display", "sans-serif"],
+        stix: ["STIX Two Text", "serif"],
+        bellefair: ["Bellefair", "serif"],
+        poppin: ["Poppins", "sans-serif"],
+        baskerville: ["Baskervville", "serif"],
+        rubik: ["Rubik", "sans-serif"],
+        cardo: ["Cardo", "serif"],
       },
     },
   },
