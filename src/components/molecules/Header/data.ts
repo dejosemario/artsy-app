49c9cb7a63adf4f50eaf5352ca6fb8161d3abb13
dejosemario <josemariaodumodu@gmail.com>
2024-02-01 21:20:56 +0100

    feat: create the headers component and setup the mobile Navlinks

---
+++ src/components/molecules/Header/data.ts
@@ -0,0 +1,19 @@
+
+export const navLinks = [
+    {
+        name: 'Home',
+        urlPath: '/',
+    },
+    {
+        name: 'Marketplace',
+        urlPath: '/marketplace',
+    },
+    {
+        name: 'Auctions',
+        urlPath: '/auctions',
+    },
+    {
+        name: 'Drop',
+        urlPath: '/drop',
+    }
+] as const;
