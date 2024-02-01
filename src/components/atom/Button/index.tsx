026a72433487198192415646d3f887cab17be5af
dejosemario <josemariaodumodu@gmail.com>
2024-02-01 21:19:17 +0100

    fix: add up the properties needed in the button

--- src/components/atom/Button/index.tsx
+++ src/components/atom/Button/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react'
+import { AiOutlineRight } from "react-icons/ai";
 
-export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  loading?: boolean;
-  variant?: "primary" | "gradient";
-  renderAs?: "button" | "link";
-  href?: string;
+type ButtonProps = {
+  children: React.ReactNode;
+  withIcon?: boolean | false;
+  outlined?: boolean;
+  hover?: boolean | true;
+  className?: string;
+}
+
+export const Button = ({
+  children,
+  withIcon,
+  outlined,
+  hover,
+  className,
+}: ButtonProps) => {
+  return (
+    <button
+      className={` ${
+        outlined ? " text-black bg-white" : "text-white"
+      } py-2 px-4 rounded-full flex flex-row items-center bg-[#3671E9] ${className}`}
+    >
+      <span className={`${hover && "hover:opacity-100"} `}>{children}</span>
+      {withIcon && (
+        <span className={"bg-white p-1 ml-4 rounded-full text-[#3671E9] "}>
+          <AiOutlineRight />
+        </span>
+      )}
+    </button>
+  );
 };
 
 
-
-
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-
-    
-  )
