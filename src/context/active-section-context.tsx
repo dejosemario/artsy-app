c20d4056090a0e27596e2f178f49501dfeb543a9
dejosemario <josemariaodumodu@gmail.com>
2024-02-03 19:07:46 +0100

    feat: create activeSecionContext

---
+++ src/context/active-section-context.tsx
@@ -0,0 +1,27 @@
+import React, { useState, createContext } from "react";
+import { navLinks } from "../components/molecules/Header/data";
+
+type SectionName = (typeof navLinks)[number]["name"];
+
+type ActiveSectionContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type ActiveSectionContextType = {
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+};
+const ActiveSectionContext = createContext<ActiveSectionContextType | null>(
+  null
+);
+
+export default function ActiveSectionContextProvider({
+  children,
+}: ActiveSectionContextProviderProps) {
+  const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  return (
+    <ActiveSectionContext.Provider value={{ activeSection, setActiveSection }}>
+      {children}
+    </ActiveSectionContext.Provider>
+  );
+}
