695d6dbc257baf0fa28cf4bbf06d026d255172fc
dejosemario <josemariaodumodu@gmail.com>
2024-02-04 21:01:07 +0100

    feat: creat the data files

---
+++ src/base/libs/data.ts
@@ -0,0 +1,38 @@
+
+export const navLinks = [
+    {
+        name: 'Home',
+        urlPath: '/',
+    },
+    {
+        name: 'Marketplace',
+        urlPath: '/marketplace',
+    },
+    {
+        name: 'Auctions',
+        urlPath: '/auctions',
+    },
+    {
+        name: 'Drop',
+        urlPath: '/drop',
+    }
+] as const;
+
+export const infoData = [
+    {
+        name: 'Blog',
+        urlPath: '/#',
+    },
+    {
+        name: 'Wallets',
+        urlPath: '/#',
+    },
+    {
+        name: 'Rates',
+        urlPath: '/#',
+    },
+    {
+        name: 'High bids',
+        urlPath: '/#',
+    }
+] as const;
