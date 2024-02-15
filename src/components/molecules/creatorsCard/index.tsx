e7e75cee7ac30545f1cd1ad5803943fad914efbd
dejosemario <josemariaodumodu@gmail.com>
2024-02-15 20:46:00 +0100

    style: sort it's responsivness

--- src/components/molecules/creatorsCard/index.tsx
+++ src/components/molecules/creatorsCard/index.tsx
@@ -1,20 +1,22 @@
 // imoort 
+'use client'
+import FadeImg from "@/components/atom/fadeImg";
 
 
 export default function CreatorsCard() {
   return (
-    <section className="bg-[#e2e2e2] mb-[66px] md:mb-[147px]">
-      <div className="container font-clash mx-auto md:py-[75px] relative">
+    <section className="bg-[#e2e2e2] mb-[66px] lg:mb-[147px] w-full">
+      <div className="container font-clash mx-auto lg:py-[75px] relative">
         <div className="overflow-hidden relative">
           <article className="flex justify-between">
-            <h2 className="font-semibold mt-[7px] md:mt-0 text-[1.5rem] md:text-[55px] leading-[155%] text-[#161616] max-w-[256px] md:max-w-[535px]">
+            <h2 className="font-semibold mt-[7px] lg:mt-0 text-[1.5rem] lg:text-[55px] leading-[155%] text-[#161616] max-w-[256px] lg:max-w-[535px]">
               TOP CREATORS OF THE WEEK
             </h2>
             <div className="flex">
-              <div className="hidden md:block rounded-full w-2.5 bg-[#AEAEAE] border mr-[34px] overflow-hidden flex items-end">
+              <div className="hidden lg:flex rounded-full w-2.5 bg-[#AEAEAE] border mr-[34px] overflow-hidden  items-end">
                 <div className="w-full h-1/5 bg-grey-dark rounded-full"></div>
               </div>
-              <ul className="absolute -right-24 md:relative md:right-0  text-grey text-[10px] md:text-[2.5rem] leading-[155%] mr-8 flex md:block gap-4">
+              <ul className="absolute -right-24 lg:relative lg:right-0  text-grey text-[10px] lg:text-[2.5rem] leading-[155%] mr-8 flex lg:block gap-4">
                 <li className="mb-5">Editorial</li>
                 <li className="mb-5">Fashion</li>
                 <li className="mb-5">Lifestyle</li>
@@ -22,22 +24,23 @@ export default function CreatorsCard() {
               </ul>
             </div>
           </article>
-          <p className="text-[13px] md:text-3xl relative leading-[130%] text-black/[0.57] font-[200] md:mr-[12rem] mt-9">
+          <p className="text-[13px] lg:text-3xl relative leading-[130%] text-black/[0.57] font-[200] lg:mr-[12rem] mt-9">
             “Everything always looked better in black and white. Everything
             always as if it were the first time; there’s always more people in a
             black and white photograph. It just makes it seem that there were
             more people at a gig, more people at a football match, than with
             colour photography. Everything looks more exciting.”– Jack Lowden
-            <span className="text-grey text-[2rem] md:text-[3rem] leading-[155%] font-bold absolute bottom-8 right-0 md:right-8">
+            <span className="text-grey text-[2rem] lg:text-[3rem] leading-[155%] font-bold absolute bottom-8 right-0 lg:right-8">
               CIRCA
             </span>
           </p>
-          <div className="flex md:my-[75px]">
-            <p className="my-4 ml-auto md:mr-8 font-bold text-[4rem] md:text-[170px] text-[#161616] md:leading-[150px] line-through decoration-8">
+          <div className="flex lg:my-[75px]">
+            <p className="my-4 ml-auto lg:mr-8 font-bold text-[4rem] lg:text-[170px] text-[#161616] lg:leading-[150px] line-through decoration-8">
               1985
             </p>
           </div>
         </div>
+        <FadeImg />
       </div>
     </section>
   );
--- src/components/molecules/featuredCard/index.tsx
+++ src/components/molecules/featuredCard/index.tsx
@@ -8,15 +8,15 @@ import Link from "next/link";
 
 export default function FeaturedCard() {
   return (
-    <section className="container">
-      <h2 className="font-bold md:font-medium text-[1.75rem] lg:text-[3rem] leading-[4rem] lg:pb-9 pb-4">
+    <section className="container pb-40">
+      <h2 className="font-bold lg:font-medium text-[1.75rem] lg:text-[3rem] leading-[4rem] lg:pb-9 pb-4">
         Featured products
       </h2>
       {articles.map(({ title, body, img, mobileImg }, index) => (
         <div
           key={index}
           className={`
-            flex flex-col items-start gap-x-8 border-t-grey border-t-[0.5px] py-8 md:py-[60px] border-b-grey border-b-[0.5px] ${
+            flex flex-col items-start gap-x-8 border-t-grey border-t-[0.5px] py-8 lg:py-[60px] border-b-grey last:border-none border-b-[0.5px] ${
               index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
             }`}
         >
@@ -33,7 +33,7 @@ export default function FeaturedCard() {
               {/* Content to show on hover */}
               <Link
                 href={"/marketplace"}
-                className="lg:flex lg:items-center text-white mb-6 ml-auto mr-10 md:m-0"
+                className="lg:flex lg:items-center text-white mb-6 ml-auto mr-10 lg:m-0"
               >
                 <span className="absolute top-4 left-1/2 -translate-x-1/2 lg:hidden   text-3xl font-semibold  w-full text-center">
                   {title}
@@ -58,10 +58,10 @@ export default function FeaturedCard() {
           </div>
 
           <article className="flex flex-col flex-1 gap-y-6 ">
-            <h4 className="text-[30px] md:text-[2.5rem] w-full text-center font-semibold  lg:font-normal  text-grey hidden lg:flex">
+            <h4 className="text-[30px] lg:text-[2.5rem] w-full text-center font-semibold  lg:font-normal  text-grey hidden lg:flex">
               {title}
             </h4>
-            <p className="text-[15px] lg:text-2xl md:text-xl mr-9 lg:mr-0 text-[#616161] lg:text-grey my-4 lg:my-0 lg:max-w-[540px]">
+            <p className="text-[15px] lg:text-2xl lg:text-xl mr-9 lg:mr-0 text-[#616161] lg:text-grey my-4 lg:my-0 lg:max-w-[540px]">
               {body}
             </p>
             <div className="flex justify-between">
