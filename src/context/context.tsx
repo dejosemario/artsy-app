48625080596e3ba3737d7ea8ec952c8705239c68
dejosemario <josemariaodumodu@gmail.com>
2024-02-16 21:24:13 +0100

    feat: create a context provider to manage the datas recieved from an api

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,3 +1,24 @@
-    
-import React, { useState, useEffect } from "react";
-// const Context = React.createContext();
+"use client";
+import React, { useState, useEffect, createContext, useContext } from "react";
+
+type ContextProviderProps = {
+  children: React.ReactNode;
+};
+function ContextProvider({ children }: ContextProviderProps) {
+  const url =
+    "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/439bc2dd8693b490539eae236918f4a53dd17457";
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [favorite, setFavorite] = useState([]);
+  const [name, setName] = useState("");
+
+  //provide context for the products using the above url
+  useEffect(() => {
+    fetch(`${url}/products.json`)
+      .then((result) => result.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  
+
+}
