e0d9819031dd163d15d1b5052fb05b3d48089b81
dejosemario <josemariaodumodu@gmail.com>
2024-03-02 21:10:51 +0100

    feat: create productCard component

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -1,7 +1,10 @@
+import SideNav from '@/components/molecules/sideNav'
 import React from 'react'
 
 export default function Marketplace() {
   return (
-    <section className='container'>this is the MarketPlace</section>
+    <section className='container'>
+      <SideNav />
+    </section>
   )
 }
