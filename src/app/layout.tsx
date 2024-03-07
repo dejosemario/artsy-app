41c0816457888d027389541963d3254ce1c4508a
dejosemario <josemariaodumodu@gmail.com>
2024-03-07 22:13:26 +0100

    fix: sort out the dataContextProvider Error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,9 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import Header from "@/components/molecules/header";
-
+import DataSectionContextProvider from "@/context/context";
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export const metadata = {
   title: "Artsy App",
@@ -22,10 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body>
-      <ActiveSectionContextProvider>
-          <Header />
-          {children}
-          <Footer />
+        <ActiveSectionContextProvider>
+          <DataSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
+          </DataSectionContextProvider>
         </ActiveSectionContextProvider>
       </body>
     </html>
--- src/context/context.tsx
+++ src/context/context.tsx
@@ -5,7 +5,22 @@ type DataSectionContextProviderProps = {
   children: React.ReactNode;
 };
 
-export default function DataSectionContextProvider({ children }: DataSectionContextProviderProps) {
+type DataSectionContextType = {
+  products: any;
+  setProducts: React.Dispatch<React.SetStateAction<any>>;
+  cart: any;
+  setCart: React.Dispatch<React.SetStateAction<any>>;
+  favorite: any;
+  setFavorite: React.Dispatch<React.SetStateAction<any>>;
+  name: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const DataSectionContext = createContext<DataSectionContextType | null>(null);
+
+export default function DataSectionContextProvider({
+  children,
+}: DataSectionContextProviderProps) {
   const url =
     "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/439bc2dd8693b490539eae236918f4a53dd17457";
   const [products, setProducts] = useState([]);
@@ -19,26 +34,33 @@ export default function DataSectionContextProvider({ children }: DataSectionCont
       .then((result) => result.json())
       .then((data) => setProducts(data))
       .catch((error) => console.log(error));
-      
   }, []);
 
-  const DataSectionContext = createContext<null>(null);
 
   return (
-    <DataSectionContext.Provider value={{ products, setProducts, cart, setCart, favorite, setFavorite, name, setName }}>
-        {children}
+    <DataSectionContext.Provider
+      value={{
+        products,
+        setProducts,
+        cart,
+        setCart,
+        favorite,
+        setFavorite,
+        name,
+        setName,
+      }}
+    >
+      {children}
     </DataSectionContext.Provider>
-  )
-
-
+  );
 }
 
-export  function  useDataSectionContext(){
+export function useDataSectionContext() {
   const context = useContext(DataSectionContext);
-  if(context === null){
+  if (context === null) {
     throw new Error(
       "useDataSectionContext must be used within a DataSectionContextProvider"
-    )
+    );
   }
   return context;
-}   
+}
