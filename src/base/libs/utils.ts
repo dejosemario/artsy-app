195838530af768e9587c7a9b89d6731f15c74b9b
dejosemario <josemariaodumodu@gmail.com>
2024-10-29 22:26:18 +0100

    chore: create toggleContent fn

--- src/base/libs/utils.ts
+++ src/base/libs/utils.ts
@@ -1,8 +1,11 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]){
-    return twMerge(clsx(inputs));
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
 
-
+export function toggleContent(e: any) {
+  e.currentTarget.lastElementChild.classList.toggle("rotate-180");
+  e.currentTarget.nextElementSibling.classList.toggle("h-0");
+}
